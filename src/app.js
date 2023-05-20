affe5a261b28cc2408cc9bca5763f943a8fe6cb1
Drancer55 <antonio281255@gmail.com>
2023-05-19 23:36:11 -0600

    first commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { PORT } from '../config';
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Bienvenido al servidor')
+})
+
+app.listen(PORT);
+console.log('Server on port', PORT);
