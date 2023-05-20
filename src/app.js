abfff41feeca6f043ac22743fe991109758adf08
Drancer55 <antonio281255@gmail.com>
2023-05-20 01:17:18 -0600

    added all setting to connect with OpenAI

--- src/app.js
+++ src/app.js
@@ -1,10 +1,32 @@
 import express from 'express';
-import { PORT } from './config.js';
+import OpenAI from 'openai';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { PORT, DB_APIKEY } from './config.js';
+
+const { Configuration, OpenAIApi } = OpenAI;
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Bienvenido al servidor')
-})
+const configuration = new Configuration({
+    organization: "org-Ocpm89EaNSJCFSEEKhvWpfqc",
+    apiKey: DB_APIKEY,
+});
 
-app.listen(PORT);
-console.log('Server on port', PORT);
+const openai = new OpenAIApi(configuration);
+
+app.use(bodyParser.json())
+app.use(cors());
+app.post('/', async (req, res) => {
+    const { prompt } = req.body;
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `${prompt}`,
+        max_tokens: 512,
+        temperature: 1,
+    });
+    console.log(response.data);
+    if (response.data.choices[0].text) {
+        res.json({message: response.data.choices[0].text})
+    }
+});
+app.listen(PORT, () => console.log('Server running on port: ', PORT));
--- src/config.js
+++ src/config.js
@@ -1,7 +1,3 @@
 export const PORT = process.env.PORT || 3001;
 
-// export const DB_HOST = process.env.PORT || 'localhost';
-// export const DB_USER = process.env.DB_USER || 'root';
-// export const DB_PASSWORD = process.env.DB_PASSWORD || "drancerpassword";
-// export const DB_NAME = process.env.DB_NAME || 'usersdb';
-// export const DB_PORT = process.env.DB_PORT || 3306;
+export const DB_APIKEY = process.env.DB_APIKEY || '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';
